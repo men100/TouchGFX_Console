77925ab20d8bc838140163be7096f94b1d7c6388
men100 <men100per@gmail.com>
2024-07-03 15:34:13 +0900

    windows11: Handle INFO, VLUP, VLDN Command

--- windows11/Program.cs
+++ windows11/Program.cs
@@ -1,6 +1,8 @@
-﻿using System.Diagnostics;
+﻿using System.IO.Ports;
+using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Management;
+using System.Timers;
 using System.Windows.Forms;
 using NAudio.CoreAudioApi;
 
@@ -8,6 +10,33 @@ namespace windows11
 {
     internal class Program
     {
+        static SerialPort? serialPort;
+        static byte volume = 0;
+        static Process? amazonMusicProcess;
+        static Process? volumerProcess;
+
+
+        struct Packet
+        {
+            public byte size;
+            public byte version;
+            public byte result;
+            public byte volume;
+        }
+
+        // 構造体をbyte[]に変換
+        static byte[] StructToBytes<T>(T str) where T : struct
+        {
+            int size = Marshal.SizeOf(str);
+            byte[] arr = new byte[size];
+
+            IntPtr ptr = Marshal.AllocHGlobal(size);
+            Marshal.StructureToPtr(str, ptr, true);
+            Marshal.Copy(ptr, arr, 0, size);
+            Marshal.FreeHGlobal(ptr);
+            return arr;
+        }
+
         static void memory(System.Management.ManagementObject mo)
         {
             decimal totalVisibleMemorySize = decimal.Parse(mo["TotalVisibleMemorySize"].ToString());
@@ -20,51 +49,170 @@ namespace windows11
             Console.WriteLine("usage: {0:0}%", usePhysicalMemory / totalVisibleMemorySize * 100);
         }
 
+        // 音量上げる
+        static void VolumeUp()
+        {
+            if (volumerProcess != null)
+            {
+                SetForegroundWindow(volumerProcess.MainWindowHandle);
+                // Alt + F12
+                System.Windows.Forms.SendKeys.SendWait("%{F12}");
+            }
+        }
+
+        // 音量下げる
+        static void VolumeDown()
+        {
+            if (volumerProcess != null)
+            {
+                SetForegroundWindow(volumerProcess.MainWindowHandle);
+                // Alt + F11
+                System.Windows.Forms.SendKeys.SendWait("%{F11}");
+            }
+        }
+
+        // データ受信イベントハンドラ
+        static void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            string data = sp.ReadExisting();  // 受信データを読み込む
+            Console.WriteLine("Received: " + data);
+
+            Packet packet = new Packet();
+            packet.size = 4;
+            packet.version = 0;
+            packet.result = 0;
+            packet.volume = volume;
+
+            byte[] packetBytes = StructToBytes(packet);
+            if (serialPort != null)
+            {
+                switch (data)
+                {
+                    case "INFO":
+                        serialPort.Write(packetBytes, 0, packetBytes.Length);
+                        break;
+
+                    case "VLUP":
+                        VolumeUp();
+                        break;
+
+                    case "VLDN":
+                        VolumeDown();
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid Command. So do nothing");
+                        break;
+                }
+            }
+        }
+
+        // タイマーイベントハンドラ
+        private static void OnTimedEvent(Object? source, ElapsedEventArgs e)
+        {
+            // 1秒ごとに実行したい処理をここに記述
+            Console.WriteLine("The Elapsed event was raised at {0:HH:mm:ss.fff}", e.SignalTime);
+
+            // システム全体の音量設定値を取得
+            {
+                // デフォルトの音声出力デバイスを取得
+                var enumerator = new MMDeviceEnumerator();
+                var device = enumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Console);
+
+                // システムの音量設定値を0～1の範囲で取得
+                float systemVolume = device.AudioEndpointVolume.MasterVolumeLevelScalar;
+
+                // 音量をパーセンテージ(0～100)に変換して表示
+                Console.WriteLine($"System volume: {systemVolume * 100:0}%");
+
+                volume = (byte)(systemVolume * 100);
+            }
+        }
+
         [DllImport("user32.dll")]
         private static extern bool SetForegroundWindow(IntPtr hWnd);
         static void Main(string[] args)
         {
-            // 現在実行中のすべてのプロセスを取得
-            Process[] processlist = Process.GetProcesses();
-            Array.Sort(processlist, new ProcComparator());
-            Process amazonMusic = null;
+            // 1秒間隔のタイマーを作成
+            System.Timers.Timer timer = new System.Timers.Timer(1000);
+            timer.Elapsed += OnTimedEvent;
+            timer.Start();
 
-            foreach (Process p in processlist)
+            // STLink の Port を探す
+            string portName = "";
+            string stLinkDescription = "STMicroelectronics STLink Virtual COM Port";
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM Win32_PnPEntity WHERE Caption LIKE '%(COM%'");
+            foreach (ManagementObject obj in searcher.Get())
             {
-                if (p.MainWindowTitle == "Amazon Music")
+                string caption = obj["Caption"].ToString().Split('(', ')')[1];
+                string description = obj["Description"].ToString();
+
+                if (description == stLinkDescription)
                 {
-                    // プロセス名とIDを出力
-                    Console.WriteLine("プロセス名: {0} ID: {1} Title: {2}", p.ProcessName, p.Id, p.MainWindowTitle);
-                    amazonMusic = p;
+                    portName = caption;
+                    Console.WriteLine($"Found! Port: {portName}");
                     break;
                 }
             }
 
+            // 操作に必要なプロセスハンドルを取得しておく
+            {
+                // 現在実行中のすべてのプロセスを取得
+                Process[] processlist = Process.GetProcesses();
+                Array.Sort(processlist, new ProcComparator());
+
+                foreach (Process p in processlist)
+                {
+                    if (p.MainWindowTitle == "Amazon Music")
+                    {
+                        // プロセス名とIDを出力
+                        Console.WriteLine("プロセス名: {0} ID: {1} Title: {2}", p.ProcessName, p.Id, p.MainWindowTitle);
+                        amazonMusicProcess = p;
+                    }
+                    if (p.ProcessName == "Volumer")
+                    {
+                        // プロセス名とIDを出力
+                        Console.WriteLine("プロセス名: {0} ID: {1}", p.ProcessName, p.Id);
+                        volumerProcess = p;
+                    }
+                }
+            }
+
+            if (portName != null)
+            {
+                serialPort = new SerialPort();
+
+                // シリアルポートの設定
+                serialPort.PortName = portName;     // 使用するCOMポート名
+                serialPort.BaudRate = 115200;       // ボーレート
+                serialPort.Parity = Parity.None;    // パリティビット
+                serialPort.DataBits = 8;            // データビット
+                serialPort.StopBits = StopBits.One; // ストップビット
+
+                serialPort.Open();  // シリアルポートを開く
+
+                // データ受信イベントハンドラを登録
+                serialPort.ReceivedBytesThreshold = 4;
+                serialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+
+                serialPort.Close();  // シリアルポートを閉じる
+            }
+
+            timer.Stop();
+            timer.Dispose();
+
+            /*
             // Space Key を送り、Play/Pause する
             if (amazonMusic != null)
             {
                 SetForegroundWindow(amazonMusic.MainWindowHandle);
                 System.Windows.Forms.SendKeys.SendWait(" ");
             }
-
-            // Volumer を経由してボリュームを操作するには
-            // p.ProcessName == "Volumer" でマッチする
-            // 音量上げる: System.Windows.Forms.SendKeys.SendWait("%{F12}");
-            // 音量下げる: System.Windows.Forms.SendKeys.SendWait("%{F11}");
-
-            // ScreenInStyle を経由して EV2785 のソースを操作するには
-            // p.ProcessName == "ScreenInStyle" でマッチする
-            // DisplayPort: System.Windows.Forms.SendKeys.SendWait("^+1");
-            // HDMI1: System.Windows.Forms.SendKeys.SendWait("^+2");
-
-            // システム全体の音量設定値を取得
-            // デフォルトの音声出力デバイスを取得
-            //var enumerator = new MMDeviceEnumerator();
-            //var device = enumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Console);
-            // システムの音量設定値を0～1の範囲で取得
-            //float systemVolume = device.AudioEndpointVolume.MasterVolumeLevelScalar;
-            // 音量をパーセンテージ(0～100)に変換して表示
-            //Console.WriteLine($"System volume: {systemVolume * 100:0}%");
+            */
 
             // CPU 使用率を取得
             /*
